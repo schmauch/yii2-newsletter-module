d5e835257aefb4d5ea10dca298fcaa83c19ebfe8
schmauch <mail@roger-schmutz.ch>
2022-12-15 14:36:34 +0100

    Blacklist Action

---
+++ actions/BlacklistAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace schmauch\newsletter\actions;
+
+use yii\base\Action;
+
+use schmauch\newsletter\models\NewsletterBlacklist;
+
+class BlacklistAction extends Action
+{
+    public $queue;
+
+
+    /**
+     * Info about queue status.
+     */
+    public function run($email = null)
+    {
+        $this->controller->viewPath = '@schmauch/yii2-newsletter-module/views/blacklist/';
+        
+        $email = $this->controller->request->post('NewsletterBlacklist')['email'] ?? $email;
+        
+        // if the address is already blacklisted, show success page
+        if ($email && $model = NewsletterBlacklist::findOne(['email' => $email])) {
+            return $this->controller->render('success', ['model' => $model]);
+        }
+        
+        // 
+        $model = new NewsletterBlacklist();
+        $model->email = $email;
+        
+        if ($this->controller->request->isPost) {
+            $model->added_at = date('Y-m-d H:i:s');
+            if(!$model->validate()) {
+                $message = 'Die E-Mail-Adresse ' . $model->email . ' konnte nicht aus der Liste entfernt werden.';
+                $message .= $model->getErrorSummary();
+                \Yii::$app->session->setFlash('error',  $message);
+            }
+
+            if($model->save()) {
+                return $this->controller->redirect(['sign-off', 'email' => $email]);
+            }
+        }        
+
+        return $this->controller->render('form', [
+            'model' => $model,
+        ]);
+    }
+
+}
