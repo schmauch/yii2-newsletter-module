d8416108ba5c74befe8b957bc36b82075936d276
schmauch <mail@roger-schmutz.ch>
2022-12-15 16:10:02 +0100

    redirect button on success site

--- views/blacklist/success.php
+++ views/blacklist/success.php
@@ -2,3 +2,7 @@
 Die E-Mail-Adresse <strong><?=$model->email?></strong> wurde
 erfolgreich aus der Liste entfernt.
 </div>
+
+<div>
+    <a href="https://www.girardi.ch" class="btn btn-dark">Zurück zur Homepage</a>
+</div>
