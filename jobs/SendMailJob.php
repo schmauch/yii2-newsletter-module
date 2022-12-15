513d59a005cac7aae389e8df0bd4ce80cee93ef4
schmauch <mail@roger-schmutz.ch>
2022-12-15 11:53:19 +0100

    layouts changed

--- jobs/SendMailJob.php
+++ jobs/SendMailJob.php
@@ -94,8 +94,10 @@ class SendMailJob extends BaseObject implements \yii\queue\JobInterface
         $message->setTo($this->recipient['email']);
         $message->setSubject($newsletter->subject);
         
-        $message->view->params['title'] = $newsletter->subject;
-        $message->view->params['email'] = $this->recipient['email'];
+        
+        // make subject and email available in template
+        $mailer->view->params['title'] = $newsletter->subject;
+        $mailer->view->params['email'] = $this->recipient['email'];
 
         foreach($attachments as $attachment) {
             $message->attach($attachment);
